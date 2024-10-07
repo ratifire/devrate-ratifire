6fc979ce29040ec320e3467cdf9db45d7eeedad5
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-10-08 00:11:52 +0200

    New feature: header added

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # RATIFIRE
 
-**__________Landing page**__________
+******\_\_****Landing page******\_\_****
 
 Figma [Template](https://www.figma.com/design/tbUhOJY9jrpsKZpbMNmCxX/Ratifire?node-id=158-26&node-type=frame&t=ktKckwbvsUt9BnZ7-0)
--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,63 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const fileinclude = require('gulp-file-include');
-const clean = require('gulp-clean');
-const server = require('gulp-server-livereload');
-const fs = require('fs');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const fileinclude = require("gulp-file-include");
+const clean = require("gulp-clean");
+const server = require("gulp-server-livereload");
+const fs = require("fs");
 
 const startServer = () => {
-    return gulp.src('./dist/')
-        .pipe(server({
-            livereload: true,
-            open: true
-        }));
+  return gulp.src("./dist/").pipe(
+    server({
+      livereload: true,
+      open: true,
+    }),
+  );
 };
 
 const cleanFiles = function (done) {
-    if (fs.existsSync('./dist/')) {
-        return gulp.src('./dist/', {read: false})
-            .pipe(clean());
-    }
-   done();
+  if (fs.existsSync("./dist/")) {
+    return gulp.src("./dist/", { read: false }).pipe(clean());
+  }
+  done();
 };
 const htmlCompile = () => {
-    return gulp.src('./src/pages/*.html')
-        .pipe(fileinclude({
-                prefix: '@@',
-                basepath: '@file'
-            }
-        ))
-        .pipe(gulp.dest('./dist/'));
+  return gulp
+    .src("./src/pages/*.html")
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file",
+      }),
+    )
+    .pipe(gulp.dest("./dist/"));
 };
 
 const scssCompile = () => {
-    return gulp.src('./src/scss/pages/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./dist/css/'));
+  return gulp
+    .src("./src/scss/pages/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./dist/css/"));
 };
 
 const images = () => {
-    return gulp.src('./src/img/**/*')
-        .pipe(gulp.dest('./dist/img/'));
+  return gulp.src("./src/img/**/*").pipe(gulp.dest("./dist/img/"));
 };
 const watchers = () => {
-    gulp.watch('./src/pages/**/*.html', gulp.parallel(htmlCompile));
-    gulp.watch('./src/scss/**/*.scss', {events: 'change'}, gulp.parallel(scssCompile));
-    gulp.watch('./src/img/**/*',{ events: ['add', 'unlink']}, gulp.parallel(images));
+  gulp.watch("./src/pages/**/*.html", gulp.parallel(htmlCompile));
+  gulp.watch(
+    "./src/scss/**/*.scss",
+    { events: "change" },
+    gulp.parallel(scssCompile),
+  );
+  gulp.watch(
+    "./src/img/**/*",
+    { events: ["add", "unlink"] },
+    gulp.parallel(images),
+  );
 };
 
-
-exports.default = gulp.series(cleanFiles,
-    gulp.parallel(htmlCompile, scssCompile, images),
-    gulp.parallel(startServer, watchers)
-    );
+exports.default = gulp.series(
+  cleanFiles,
+  gulp.parallel(htmlCompile, scssCompile, images),
+  gulp.parallel(startServer, watchers),
+);
