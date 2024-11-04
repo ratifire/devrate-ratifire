06b67023710c2e165a581d6f142ccf8f38b73f93
Ruslan <rusik.sorok1337@gmail.com>
2024-11-04 18:44:38 +0200

    fixed slider and added gaps between blocks

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,10 @@
 const teamSwiperBlock = new Swiper(".team-swiper", {
   direction: "horizontal",
-  slidesPerView: 4,
+  slidesPerView: "auto",
   loop: false,
-  spaceBetween: 15,
-
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 0,
-    },
-
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 5,
-    },
-
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 10,
-    },
-  },
 
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
   },
 });
